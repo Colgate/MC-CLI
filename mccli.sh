d259e6a60b162be603937f51cdc4e882cc4ec1be
Colgate <jeremyrchardson@gmail.com>
2014-05-01 02:11:38 -0500

    This needed some love.
    My bash skills have gotten loads better recently.

--- mccli.sh
+++ mccli.sh
@@ -1,175 +1,99 @@
 #!/bin/bash
-#
-# description: Bash CLI for minecraft servers
-# processname: MCControl
 
+#############################################################
+# name:        MC-CLI                                       # 
+# author:      Colgate                                      #
+# description: Bash CLI for minecraft server administration #
+#############################################################  
+
+
+#Server process check function
+isOnline() {
+    proc=/etc/$1.pid
+    [[ -f $proc ]] && {
+        ps -p $proc> /dev/null
+        [[ $? = 1 ]] && return 1 || return 0;
+    } || return 1
+}
+
+#Start server if not already running
 start() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo 'Starting '$1;
-                    rm -f /etc/$1.pid;
-                    screen -S $1 -d -m /servers/$1/rtoolkit.sh;
-                    screen -list | grep $1 | cut -f1 -d'.' | sed 's/\W//g' > /etc/$1.pid;
-                    timeout 20 tail -f /servers/$1/server.log;
-                else 
-                    echo 'Unholding '$1;
-                    screen -x `cat /etc/$1.pid` -p 0 -X stuff '.unhold'$'\012';
-                    timeout 20 tail -f /servers/$1/server.log;
-            fi;
-        else
-            echo 'Starting '$1;
-            rm -f /etc/$1.pid;
-            screen -S $1 -d -m /servers/$1/rtoolkit.sh;
-            screen -list | grep $1 | cut -f1 -d'.' | sed 's/\W//g' > /etc/$1.pid;
-            timeout 20 tail -f /servers/$1/server.log;
-    fi;
+    isOnline $1 && {
+        echo 'Unholding '$1;
+        screen -x `cat /etc/$1.pid` -p 0 -X stuff '.unhold'$'\012';
+        timeout 20 tail -f /servers/$1/server.log;
+    } || {
+        echo 'Starting '$1;
+        rm -f /etc/$1.pid;
+        screen -S $1 -d -m /servers/$1/rtoolkit.sh;
+        screen -list | grep $1 | cut -f1 -d'.' | sed 's/\W//g' > /etc/$1.pid;
+        timeout 20 tail -f /servers/$1/server.log;
+    }
 }
 
+#Stop the server, but do not kill it (.hold the server)
 stop() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo $1' is not running. Perhaps you meant to start it?'
-                else
-                    echo 'Stopping '$1
-                    screen -x `cat /etc/$1.pid` -p 0 -X stuff '.hold'$'\012';
-                    timeout 20 tail -f /servers/$1/server.log;
-            fi;
-        else
-            echo $1' is not running. Perhaps you meant to start it?'
-    fi;
-}
+    isOnline $1 && {
+        echo 'Stopping '$1
+        screen -x `cat /etc/$1.pid` -p 0 -X stuff '.hold'$'\012';
+        timeout 20 tail -f /servers/$1/server.log;
+    } || echo "$1 is already stopped or not running!"
+}    
+
+#Attach to server screen session/console.
 attach() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo $1' is not running. Perhaps you meant to start it?'
-                else
-                    echo 'Attaching to '$1
-                    screen -x `cat /etc/$1.pid`
-            fi;
-        else
-            echo $1' is not running. Perhaps you meant to start it?'
-    fi;
+    isOnline $1 && {
+        screen -x `cat /etc/$1.pid`
+    } || echo "$1 is not currently running!"
 }
+
+#Hard restart the server.
 restart() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo $1' is not running. Perhaps you meant to start it?'
-                else
-                    echo 'Restarting '$1
-                    screen -x `cat /etc/$1.pid` -p 0 -X stuff '.restart'$'\012';
-                    timeout 20 tail -f /servers/$1/server.log;
-            fi;
-        else
-            echo $1' is not running. Perhaps you meant to start it?'
-    fi;
+    isOnline $1 && {
+        echo 'Restarting '$1
+        screen -x `cat /etc/$1.pid` -p 0 -X stuff '.restart'$'\012';
+        timeout 20 tail -f /servers/$1/server.log;
+    } || echo "$1 is not currently running!"
+
 }
+
+#Soft restart the server. (send "reload" command)
 reload() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo $1' is not running. Perhaps you meant to start it?'
-                else
-                    echo 'Reloading '$1
-                    screen -x `cat /etc/$1.pid` -p 0 -X stuff 'reload'$'\012';
-                    timeout 20 tail -f /servers/$1/server.log;
-            fi;
-        else
-            echo $1' is not running. Perhaps you meant to start it?'
-    fi;
+    isOnline $1 && {
+        screen -x `cat /etc/$1.pid` -p 0 -X stuff 'reload'$'\012';
+        timeout 20 tail -f /servers/$1/server.log;        
+    } || echo "$1 is not currently running!"
 }
-send() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo $1' is not running. Perhaps you meant to start it?'
-                else
-                    echo 'sending command '$2' to '$1
-                    screen -x `cat /etc/$1.pid` -p 0 -X stuff $2$'\012';
-                    timeout 3 tail -n 1 -f /servers/$1/server.log;
-            fi;
-        else
-            echo $1' is not running. Perhaps you meant to start it?'
-    fi;
+
+#Execute commands without actually attaching to server console
+exec() {
+    isOnline $1 && {
+        echo 'sending command '"$2 $3 $4 $5 $6 $7 $8 $9"' to '$1
+        screen -x `cat /etc/$1.pid` -p 0 -X stuff "$2 $3 $4 $5 $6 $7 $8 $9"$'\012';
+        timeout 3 tail -n 1 -f /servers/$1/server.log;
+    } || echo "$1 is not currently running!"
 }
+
+#Completely kill server and end process
 kill() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo $1' is not running. Perhaps you meant to start it?'
-                else
-                    echo 'Killing '$1
-                    screen -x `cat /etc/$1.pid` -p 0 -X stuff '.stopwrapper'$'\012';
-                    timeout 25 tail -f /servers/$1/server.log;
-                    kill `cat /etc/$1.pid`
-                    screen -wipe
-                    rm -fv /etc/$1.pid
-            fi;
-        else
-            echo $1' is not running. Perhaps you meant to start it?'
-    fi;
-}
-status() {
-    if [ -f /etc/$1.pid ]
-        then
-            ps -p `cat /etc/$1.pid`> /dev/null;
-            if [ $? = 1 ]
-                then
-                    echo $1' is not running. Perhaps you meant to start it?'
-                else
-                    echo -e 'Checking Status of '$1'\n';
-                    normal=$(printf "\033[m")
-                    screen -x `cat /etc/$1.pid` -p 0 -X stuff 'ping'$'\012';
-                    sleep 1;
-                    tail -2 /servers/$1/server.log | grep -q "Pong!"
-                    if [ $? = 1 ]
-                        then
-                            echo 'Status: "stopped"' >> /tmp/$1.status;
-                        else
-                            echo 'Status: "running"' >> /tmp/$1.status;
-                            screen -x `cat /etc/$1.pid` -p 0 -X stuff 'lag'$'\012';
-                            sleep 0.5;
-                            echo "Uptime: \"`tail -n 20 /servers/$1/server.log | grep "Uptime:" | awk '{for(i=5;i<=NF;i++){printf "%s ",$i}}'`\"" >> /tmp/$1.status
-                            echo "TPS: \"`tail -n 20 /servers/$1/server.log | grep "TPS" | awk '{print $7}'`\"" >> /tmp/$1.status
-                            echo "Memory: \"`tail -n 20 /servers/$1/server.log | grep "Maximum" | awk '{print $6}'` Maximum | `tail -n 20 /servers/$1/server.log | grep "Free memory:" | awk '{print $6}'` Free\"" >> /tmp/$1.status
-                            screen -x `cat /etc/$1.pid` -p 0 -X stuff 'list'$'\012';
-                            sleep 0.5;
-                            echo "Players: \"`tail -n 10 /servers/$1/server.log | grep "There are" | awk '{print $6}'`\"" >> /tmp/$1.status
-                    fi;
-                awk -F'"' '{ printf "%-20s %s\n", $1, $2}' /tmp/$1.status
-                rm -f /tmp/$1.status
-                printf "\033[m"
-            fi;
-        else
-            echo $1' is not running. Perhaps you meant to start it?'
-    fi;
+    isOnline $1 && {
+        echo 'Killing '$1
+        screen -x `cat /etc/$1.pid` -p 0 -X stuff '.stopwrapper'$'\012';
+        timeout 25 tail -f /servers/$1/server.log;
+        kill `cat /etc/$1.pid`
+        screen -wipe
+        rm -f /etc/$1.pid
+    } || echo "$1 is not currently running!"
 }
 
 # See how we were called.
 case "$2" in
   start)
-	start $1
-	;;
+    start $1
+    ;;
   stop)
-	stop $1
-	;;
+    stop $1
+    ;;
   restart)
     restart $1
     ;;
@@ -179,16 +103,13 @@ case "$2" in
   reload)
     reload $1
     ;;
-  send)
-    send $1 $3
+  exec)
+    exec $1 "$3 $4 $5 $6 $7 $8"
     ;;
   kill)
     kill $1
     ;;
-  status)
-    status $1
-    ;;
 *)
-	echo $"Usage: $0 %SERVERNAME% {start|stop|attach|restart|reload|send|kill|status}"
-	exit 1
+    echo $"Usage: $0 %SERVERNAME% {start|stop|attach|restart|reload|exec|kill}"
+    exit 1
 esac
